c3afc247d55371c3ed30be032f527d9006db1a8b
SW <sw3159@columbia.edu>
2020-04-18 13:43:13 -0400

    overload builtin functions notes

--- OOP/overload_builtin_functions.py
+++ OOP/overload_builtin_functions.py
@@ -10,9 +10,37 @@ class Order:
     def __bool__(self):
         return len(self.cart) > 0
 
+    #"+"
+    def __add__(self, other):
+        new_cart = self.cart.copy()
+        new_cart.append(other)
+        return Order(new_cart, self.customer)
+
+    #"+="
+    def __iadd__(self, other):
+        self.cart.append(other)
+        return self
+
+    # indexing and slicing with []
+    def __getitem__(self, key):
+        return self.cart[key]
+
+    # reverse operators
+    def __radd__(self, other):
+        new_cart = self.cart.copy()
+        new_cart.insert(0, other)
+        return Order(new_cart, self.customer)
+
 order = Order(['banana', 'apple', 'orange'], 'Cheng')
 print(len(order))
 print(bool(order))
+
+order += "grape"
+print(order.cart)
+print(order[-1])
+
+order = "grapefruit" + order 
+print(order.cart)
 
 order2 = Order([], 'Python')
 
@@ -21,6 +49,10 @@ for order in [order, order2]:
         print(f"{order.customer}'s order is processing..." )
     else:
         print(f"Empty order for customer {order.customer}")
+
+print((order + "grapes").customer)
+order = order + 'mango'
+print(order.cart)
 
 #abs()
 class Vector:
@@ -50,3 +82,74 @@ class Vector2:
 v = Vector2(3, 4)
 print(str(v))
 print(repr(v))
+
+
+#### A Complete Example
+from math import hypot, atan, sin, cos 
+
+class CustomComplex:
+    def __init__(self, real, imag):
+        self.real = real 
+        self.imag = imag 
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.real}, {self.imag})"
+
+    def __str__(self):
+        return f"({self.real}{self.imag:+}j)"
+
+    def __add__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            real_part = self.real + other 
+            imag_part = self.imag
+
+        if isinstance(other, CustomComplex):
+            real_part = self.real + other.real 
+            imag_part = self.imag + other.imag
+
+        return self.__class__(real_part, imag_part)
+
+    def __sub__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            real_part = self.real - other 
+            imag_part = self.imag 
+
+        if isinstance(other, CustomComplex):
+            real_part = self.real - other.real 
+            imag_part = self.imag - other.imag
+
+        return self.__class__(real_part, imag_part)
+
+    def __mul__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            real_part = self.real * other 
+            imag_part = self.imag * other  
+
+        if isinstance(other, CustomComplex):
+            real_part = (self.real * other.real) - (self.imag * other.imag)
+            imag_part = (self.real * other.imag) + (self.imag * other.real)
+
+        return self.__class__(real_part, imag_part)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rsub__(self, other):
+        #x - y != y - x 
+        if isinstance(other, float) or isinstance(other, int):
+            real_part = other - self.real 
+            imag_part = -self.imag
+
+        return self.__class__(real_part, imag_part)
+
+    def __eq__(self, other):
+        return (self.real == other.real) and (self.imag == other.imag)
+
+    def __ne__(self, other):
+        return (self.real != other.real) or (self.imag != other.imag)
+   
+
+
