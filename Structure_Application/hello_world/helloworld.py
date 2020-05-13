bccfddbd4448f876ba7f16ce1ca9c03ab1ad6eb2
SW <sw3159@columbia.edu>
2020-05-13 13:26:21 -0400

    import from utils.py

--- Structure_Application/hello_world/helloworld.py
+++ Structure_Application/hello_world/helloworld.py
@@ -4,12 +4,13 @@ __version__ = '0.1.0'
 
 import re 
 import requests
+from utils import show_message
 
 URL = "https://en.wikipedia.org/wiki/%22Hello,_World!%22_program"
 
 def do_hello():
     res = requests.get(URL)
-    print(re.findall('<title>(.*?)</title>', res.text)[0])
+    show_message(re.findall('<title>(.*?)</title>', res.text)[0])
 
 if __name__ == '__main__':
     do_hello()
